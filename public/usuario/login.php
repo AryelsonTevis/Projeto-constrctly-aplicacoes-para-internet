16e05f44535704c43b3f5f077d0dd1e67368afaa
Joao Santos <joao0188@[D[D[D[D[D_[C[C[C[C[outlook.com>
2025-09-19 16:05:03 -0300

    Feature: Create style in page login/Create User

--- public/usuario/login.php
+++ public/usuario/login.php
@@ -1,20 +1,33 @@
 <form action="/projeto/usuario/logar" method="post">
+    <link rel="stylesheet" href="/projeto/assets/css/login.css">
 
+    <div class="form-container">
 
+        <h2 class="title-card">Login</h2>
+        <label>Email:</label>
+        <input class="text-box" placeholder="Digite o seu e-mail" type="text" name="email" value="<?= (isset($parametro[0]["email"])) ? $parametro[0]["email"] : "" ?>" />
+        <br />
 
+        <label>Senha:</label>
+        <input class="text-box" placeholder="Digite sua senha" id="senha" type="password" name="senha" value="<?= (isset($parametro[0]["senha"])) ? $parametro[0]["senha"] : "" ?>"/>
+        <button class="button-view" type="button" onclick="mostrarSenha()">👁️</button>
+        <script>
+            function mostrarSenha() {
+                const campo = document.getElementById("senha");
+                if (campo.type === "password") {
+                    campo.type = "text"; 
+                } else {
+                    campo.type = "password"; 
+                }
+            }
+        </script>
+        <br />
+        <a class="forget" href="#">Esqueceu a senha?</a>
+        <div class="button-card">
 
-    <label>Email:</label>
-    <input type="text" name="email" value="<?= ($parametro != null) ? $parametro[0]["email"] : "" ?>" />
-    <br />
-
-    <label>Senha:</label>
-    <input type="password" name="senha" value="<?= ($parametro != null) ? $parametro[0]["senha"] : "" ?>" />
-    <br />
-
-
-    <input type="submit" value="Enviar">
+            <input class="button-enviar" type="submit" value="Enviar">
+            <a class="button-cadastrar" href="/projeto/usuario/formulario">Cadastrar</a>
+        </div>
+    </div>
 
 </form>
-<div class="button-container">
-    <a href="/projeto/usuario/formulario" class="button-cadastrar">Cadastrar</a>
-</div>
