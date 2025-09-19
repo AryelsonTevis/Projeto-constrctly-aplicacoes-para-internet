1cb0a1e0dceb88f79f10f591c463ce857635e0a2
Joao Santos <joao0188@[D[D[D[D[D_[C[C[C[C[outlook.com>
2025-09-18 21:59:28 -0300

    Update

--- generic/Controller.php
+++ generic/Controller.php
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace generic;
-
-class Controller
-{
-    private $arrayChamadas = [];
-    public function __construct()
-    {
-        $this->arrayChamadas = [
-            "usuario/listar" => new Acao("Usuario", "listar"),
-            "usuario/listarid" => new Acao("Usuario", "listarid"),
-            "usuario/listarpro" => new Acao("Usuario", "listarpro"),
-            "usuario/formulario" => new Acao("Usuario", "formulario"),
-            "usuario/formularioalterar" => new Acao("Usuario", "alterarForm"),
-            "usuario/inserir" => new Acao("Usuario", "inserir"),
-            "usuario/alterar" => new Acao("Usuario", "alterar"),
-            "usuario/apagar" => new Acao("Usuario", "apagar"),
-            "proprietario/listar" => new Acao("Proprietario", "listar"),
-            "proprietario/listarid" => new Acao("Proprietario", "listarid"),
-            "proprietario/formulario" => new Acao("Proprietario", "formulario"),
-            "proprietario/inserir" => new Acao("Proprietario", "inserir"),
-            "usuario/listarproprietarios" => new Acao("Usuario", "listarProprietarios")
-        ];
-    }
-
-    public function verificarChamadas($rota)
-    {   
-        if (isset($this->arrayChamadas[$rota])) {
-            $acao = $this->arrayChamadas[$rota];
-            $acao->executar();
-            return;
-        }
-        echo "Rota não existe!";
-    }
-}
-?>
-=======
 <?php
 
 namespace generic;
@@ -76,4 +37,3 @@ class Controller
         echo "Rota não existe!";
     }
 }
->>>>>>> 84aa5acded28c0dbe5e374f93d96af76e04d65d4
