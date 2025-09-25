7dcd26485c87d1ac4bc0c38e23fafd3267837afc
Joao Santos <joao0188@[D[D[D[D[D_[C[C[C[C[outlook.com>
2025-09-25 13:51:24 -0300

    Style Listar Proprietario

--- public/proprietario/listarpro.php
+++ public/proprietario/listarpro.php
@@ -1,52 +1,37 @@
-<a href="/projeto/proprietario/formulario?id=">Cadastrar</a>
+<link rel="stylesheet" href="/projeto/assets/css/listarProprietarios.css">
 
-<table>
+    
+
+<div class="button-container">
+    <a class="button-cadastrar" href="/projeto/proprietario/formulario?id=">Cadastrar</a>
+    <a class="button-exit" href="/projeto/usuario/logout">Sair</a>
+</div>
+
+<table class="tabela">
+    <h2 class="title">Proprietarios</h2>
     <tr>
         <th>ID</th>
         <th>Nome</th>
         <th>Email</th>
-        <th>telefone</th>
+        <th>Telefone</th>
         <th>CPF/CNPJ</th>
         <th>Endereço</th>
         <th>Ações</th>
-
     </tr>
-    <?php foreach ($parametro as $p) {
-
-    ?>
-    <tr>
-        <td>
-            <?= $p["proprietario_id"] ?>
-        </td>
-
-        <td>
-            <?= $p["proprietario"] ?>
-        </td>
-        <td>
-            <?= $p["email"] ?>
-        </td>
-        <td>
-            <?= $p["numero_telefone"] ?>
-        </td>
-        <td>
-            <?= $p["CPF_CNPJ"] ?>
-        </td>
-        <td>
-            <?= $p["endereço_cobrança"] ?>
-        </td>
-
-        <td><a href="/projeto/proprietario/formularioalterar?id=<?php echo $p['proprietario_id']; ?>">alterar</a>
-        </td>
-        <td><a href="/projeto/proprietario/apagar?id=<?php echo $p['proprietario_id']; ?>">apagar</a></td>
-        <td><a href="/projeto/casa/listarcasa?id=<?php echo $p['proprietario_id']; ?>">casas</a></td>
-
-    </tr>
-    <?php
-    } ?>
-
-
-
-
-
-
-</table><a href="/projeto/usuario/logout">sair</a>
+    <?php foreach ($parametro as $p) { ?>
+        <tr>
+            <td><?= $p["proprietario_id"] ?></td>
+            <td><?= $p["proprietario"] ?></td>
+            <td><?= $p["email"] ?></td>
+            <td><?= $p["numero_telefone"] ?></td>
+            <td><?= $p["CPF_CNPJ"] ?></td>
+            <td><?= $p["endereço_cobrança"] ?></td>
+            
+            <td>
+                <a class="self-acoes" href="/projeto/proprietario/formularioalterar?id=<?= $p['proprietario_id']; ?>">Alterar</a> |
+                <a class="self-acoes" href="/projeto/casa/listarcasa?id=<?= $p['proprietario_id']; ?>">Casas</a> |
+                <a class="self-acoes" href="/projeto/proprietario/apagar?id=<?= $p['proprietario_id']; ?>">Apagar</a>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
