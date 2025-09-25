dbdecb7dd62e13eab19b390b16a169d6b6b33031
Joao Santos <joao0188@[D[D[D[D[D_[C[C[C[C[outlook.com>
2025-09-25 14:59:06 -0300

    Fix

--- public/casa/formalterar.php
+++ public/casa/formalterar.php
@@ -1,27 +1,33 @@
+<link rel="stylesheet" href="/projeto/assets/css/formulario.css">
 <?php foreach ($parametro as $p) { ?>
-<tr>
-    <td>
-        <form action="/projeto/casa/alterar" method="post">
+    <tr>
+        <td>
+            <div class="form-container">
+                <h2 class="title-card">Alterar Casa</h2>
+                <form action="/projeto/casa/alterar" method="post">
 
-            <input type="hidden" name="casa_id" value="<?= $p['casa_id']; ?>">
-
-
-            <label>Bairro:</label>
-            <input type="text" name="bairro" value="<?= $p['bairro']; ?>">
-            <br>
-            <label>Metros:</label>
-            <input type="text" name="metros" value="<?= $p['metros']; ?>">
-            <br>
-            <label>Quantidade de comodos:</label>
-            <input type="text" name="comodos" value="<?= $p['quantidade_comodos']; ?>">
-            <br>
-            <label>Valor estimado: R$</label>
-            <input type="text" name="valor" value="<?= $p['valor_estimado']; ?>">
-            <br>
+                    <input type="hidden" name="casa_id" value="<?= $p['casa_id']; ?>">
 
 
-            <input type="submit" value="alterar" class="buton">
-        </form>
-    </td>
-</tr>
+                    <label>Bairro:</label>
+                    <input class="text-box" type=" text" name="bairro" value="<?= $p['bairro']; ?>">
+                    <br>
+                    <label>Metros:</label>
+                    <input class="text-box" type=" text" name="metros" value="<?= $p['metros']; ?>">
+                    <br>
+                    <label>Quantidade de comodos:</label>
+                    <input class="text-box" type=" text" name="comodos" value="<?= $p['quantidade_comodos']; ?>">
+                    <br>
+                    <label>Valor estimado: R$</label>
+                    <input class="text-box" type=" text" name="valor" value="<?= $p['valor_estimado']; ?>">
+                    <br>
+
+                    <div>
+                        <a class="button-voltar" href="/projeto/casa/listarcasa?id=<?php $p['proprietario_id']?>">Voltar</a>
+                        <input class="button-enviar" type="submit" value="Alterar" class="buton">
+                    </div>
+            </div>
+            </form>
+        </td>
+    </tr>
 <?php } ?>
